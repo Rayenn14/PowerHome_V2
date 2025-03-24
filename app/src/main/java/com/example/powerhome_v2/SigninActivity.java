5546dc636cd46aff1f69c1ba8060176761bf3c0d
Njikamcode <iris94.marienjikam@gmail.com>
2025-03-24 12:05:04 +0100

    Backend liaison des Text View

--- app/src/main/java/com/example/powerhome_v2/SigninActivity.java
+++ app/src/main/java/com/example/powerhome_v2/SigninActivity.java
@@ -1,6 +1,9 @@
 package com.example.powerhome_v2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SigninActivity extends AppCompatActivity {
@@ -8,5 +11,19 @@ public class SigninActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        TextView creerCompte = findViewById(R.id.Creeruncompteclick);
+
+        creerCompte.setOnClickListener(v -> {
+            Intent intent = new Intent(SigninActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        TextView mdpOublie = findViewById(R.id.Mdpoublieeclick);
+
+        mdpOublie.setOnClickListener(v -> {
+            Intent intent = new Intent(SigninActivity.this, ForgetpwdActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/powerhome_v2/ValidationActivity.java
+++ app/src/main/java/com/example/powerhome_v2/ValidationActivity.java
@@ -1,6 +1,9 @@
 package com.example.powerhome_v2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ValidationActivity extends AppCompatActivity {
@@ -8,5 +11,12 @@ public class ValidationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validation);
+
+        TextView goBackToFP = findViewById(R.id.recommencerClick);
+
+        goBackToFP.setOnClickListener(v -> {
+            Intent intent = new Intent(ValidationActivity.this, ForgetpwdActivity.class);
+            startActivity(intent);
+        });
     }
 }
