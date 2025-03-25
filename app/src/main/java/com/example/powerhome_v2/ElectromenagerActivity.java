217d2d9ca8096e023caf163adda4e46e74d62d79
Njikamcode <iris94.marienjikam@gmail.com>
2025-03-25 22:04:35 +0100

    avancement

---
+++ app/src/main/java/com/example/powerhome_v2/ElectromenagerActivity.java
@@ -0,0 +1,26 @@
+package com.example.powerhome_v2;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ElectromenagerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_electromenager);
+
+
+
+        ImageView addEquip = findViewById(R.id.addEquipement);
+
+        addEquip.setOnClickListener(v -> {
+            Intent intent = new Intent(ElectromenagerActivity.this, SigninActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/powerhome_v2/ResidentActivity.java
@@ -0,0 +1,22 @@
+package com.example.powerhome_v2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.powerhome_v2.R;
+import com.example.powerhome_v2.SigninActivity;
+
+public class ResidentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_residents);
+
+
+    }
+}
